201c005225777df983cfac4ab2daa5eddf4f232e
Potat-OS1 <apotat.os1@gmail.com>
2022-10-12 23:11:47 -0700

    much expanded the sudoku square, formatting followed.

--- src/main/java/com/example/sudokiu/HelloApplication.java
+++ src/main/java/com/example/sudokiu/HelloApplication.java
@@ -8,27 +8,42 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HelloApplication extends Application {
-    int numRows = 9;
-    int numColumns = 9;
+    int numRows = 100;
+    int numColumns = 100;
     int max = 0;
+    int screenx = 1000;
+    int screeny = 1000;
+    double smallestBound;
+    double margins = 0;
+    double tileSize = 0;
     static List<List<Integer>> rows = new ArrayList<>();
     static List<List<Integer>> columns = new ArrayList<>();
+    Font font;
     @Override
     public void start(Stage stage){
         BuildListsOLists();
         max = numRows;
-        if(numRows != numColumns) {
-            if (numRows % 6 < numColumns % 6) {
-                max = numColumns;
-            }
+        if((numRows != numColumns) && (numRows % 6 < numColumns % 6)) {
+            max = numColumns;
         }
+        smallestBound = screenx;
+        if((screenx != screeny) && (screenx % 6 < screeny % 6)){
+            smallestBound = screeny;
+        }
+        System.out.println(smallestBound + " is the smallest dimension and" + max + " is the largest column or row" );
+        //divide the smallest bound by 10 and allocate it to the margin total size. the margin is then cut up based on max + 1. the remaining 90% of the smallest bound is divided by max and assigned to tiles.
+        margins = (smallestBound * .1) / (max + 1);
+        tileSize = (smallestBound * .9) / (max);
+
+        font = Font.font("Arial", FontWeight.BOLD,tileSize / 1.75);
         Pane board = new Pane(CreateBoard());
-        Scene scene = new Scene(board, 1000, 1000);
+        Scene scene = new Scene(board, screenx, screeny);
         stage.setTitle("Sudoku");
         stage.setScene(scene);
         stage.show();
@@ -64,23 +79,28 @@ public class HelloApplication extends Application {
     public Node CreateBoard(){
         StackPane holder = new StackPane();
         holder.setBackground(new Background(new BackgroundFill((Color.DARKCYAN), null, null)));
-        holder.setPadding(new Insets(10, 10, 10, 10));
+        holder.setPadding(new Insets(margins, margins, margins, margins));
         VBox holderVBox = new VBox(3);
-        holderVBox.setSpacing(10);
+        holderVBox.setSpacing(margins);
         int a = 0;
         while(a < numRows){
             HBox row = new HBox();
-            row.setSpacing(10);
+            row.setSpacing(margins);
             holderVBox.getChildren().add(row);
             for(int b = 0; b < numColumns; b++){
                 StackPane Tile = new StackPane();
-                Tile.setMinSize(100, 100);
+                Tile.setMinSize(tileSize, tileSize);
                 Tile.setBackground(new Background(new BackgroundFill((Color.BISQUE), null, null)));
                 row.getChildren().add(Tile);
                 Label number = new Label();
-                number.setFont(new Font("Arial", 30));
+                number.setFont(font);
                 Tile.getChildren().add(number);
-                number.setText(String.valueOf(AddNumber(rows.get(a), columns.get(b))));
+                int num = AddNumber(rows.get(a), columns.get(b));
+                double colorRange = (0.5 / max) * (double) num;
+                double colorBound = 0.25;
+                double finalColor = colorBound + colorRange;
+                number.setText(String.valueOf(num));
+                number.setTextFill(new Color(finalColor, finalColor, 1.0, 1.0));
             }
             a++;
         }
